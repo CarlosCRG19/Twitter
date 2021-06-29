76f675d206319276d290a8799b36f5034f379ee2
CarlosCRG19 <carloscrg19@gmail.com>
2021-06-28 21:00:36 -0500

    feat (2nd Story finished): a relative timestamp for each tweet is displayed on the right top

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/adapters/TweetsAdapter.java
@@ -0,0 +1,84 @@
+package com.codepath.apps.restclienttemplate.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder>{
+
+    Context context;
+    List<Tweet> tweets;
+
+    // Pass in the context and list of tweets
+    public TweetsAdapter(Context context, List<Tweet> tweets){
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+
+    @NonNull
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+        // Get data at position
+        Tweet tweet = tweets.get(position);
+        // Bind the tweet with the view holder
+        holder.bind(tweet);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tweets.size();
+    }
+
+    // For each row, inflate the layout
+
+    // Bind values based on the position of the element
+
+    // define a viewholder
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ivProfileImage;
+        TextView tvBody;
+        TextView tvScreenName;
+        TextView tvTimestamp;
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvTimestamp = itemView.findViewById(R.id.tvTimestamp);
+        }
+
+        public void bind(Tweet tweet) {
+            tvBody.setText(tweet.body);
+            tvScreenName.setText(tweet.user.screenName);
+            tvTimestamp.setText(tweet.timestamp);
+            Glide.with(context)
+                    .load(tweet.user.profileImageUrl)
+                    .into(ivProfileImage);
+
+        }
+    }
+}
